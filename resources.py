0f719fb94846ccbcbb2b0974b6e6a84ce775c582
2ood <tommychoi0128@gmail.com>
2021-11-19 13:32:29 +0000

    feat : added simple_random
    simple_random.py generates password solely randomly.
    doesn't have seed; it is not deterministic

---
+++ resources.py
@@ -0,0 +1,8 @@
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+usualSymbols = "@#$%^&~?"
+dangerousSymbols ="!*{}[]()<>:;-_/+" 
+
+length =10
+result = ""
